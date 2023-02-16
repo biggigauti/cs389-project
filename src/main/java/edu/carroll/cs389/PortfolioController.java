8901bad2b6e8ad93269a23b1bb137492f24eb5c4
Birgir Gauti <birgirgauti@gmail.com>
2023-02-16 16:48:44 -0700

    Added logback file and modified .gitignore to ignore log files.

--- src/main/java/edu/carroll/cs389/PortfolioController.java
+++ src/main/java/edu/carroll/cs389/PortfolioController.java
@@ -1,10 +1,15 @@
 package edu.carroll.cs389;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PortfolioController {
     @GetMapping("/portfolio")
-    public String portfolio() {return "portfolio";}
+    public String index(@RequestParam(value="name", defaultValue="User")String name, Model model) {
+        model.addAttribute("name", name);
+        return "portfolio";
+    }
 }
