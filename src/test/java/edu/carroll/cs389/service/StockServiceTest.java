6e9b8e9290914b0aca041cb0dfab563381347c9b
Birgir Gauti <birgirgauti@gmail.com>
2023-03-31 12:31:54 -0600

    Set up structure for StockServiceTest and removed unused imports from UserserviceTest.

--- src/test/java/edu/carroll/cs389/service/StockServiceTest.java
+++ src/test/java/edu/carroll/cs389/service/StockServiceTest.java
@@ -1,5 +1,22 @@
 package edu.carroll.cs389.service;
 
+import jakarta.transaction.Transactional;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.*;
+
+@Transactional
+@SpringBootTest
 public class StockServiceTest {
 
+    @Autowired
+    private StockServiceImpl stockService;
+
+
+    @Test
+    private void createPositionTest() {
+        
+    }
 }
--- src/test/java/edu/carroll/cs389/service/UserServiceTest.java
+++ src/test/java/edu/carroll/cs389/service/UserServiceTest.java
@@ -1,12 +1,9 @@
 package edu.carroll.cs389.service;
 
-import edu.carroll.cs389.jpa.model.User;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.List;
 
 import static org.junit.Assert.*;
 
