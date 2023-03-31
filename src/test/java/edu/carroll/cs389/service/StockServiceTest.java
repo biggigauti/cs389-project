e01615ceb12a01fa13e6d9de349672b5dae2351a
Birgir Gauti <birgirgauti@gmail.com>
2023-03-31 12:34:10 -0600

    Started on a StockService test but I need to change some functionality elsewhere first

--- src/test/java/edu/carroll/cs389/service/StockServiceTest.java
+++ src/test/java/edu/carroll/cs389/service/StockServiceTest.java
@@ -1,5 +1,6 @@
 package edu.carroll.cs389.service;
 
+import edu.carroll.cs389.jpa.model.User;
 import jakarta.transaction.Transactional;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +15,16 @@ public class StockServiceTest {
     @Autowired
     private StockServiceImpl stockService;
 
+    User testUser = new User();
+
 
     @Test
     private void createPositionTest() {
-        
+        String ticker = "sofi";
+        Float price = 10f;
+        Float shares = 10f;
+        User user = testUser;
+
+        stockService.createPosition(ticker, price, shares, user);
     }
 }
