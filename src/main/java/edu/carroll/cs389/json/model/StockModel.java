519c307386d327f44eebd503a0e5f2410f145a30
Birgir Gauti <birgirgauti@gmail.com>
2023-04-07 13:08:51 -0600

    Added internal documentation to all json models.

--- src/main/java/edu/carroll/cs389/json/model/StockModel.java
+++ src/main/java/edu/carroll/cs389/json/model/StockModel.java
@@ -1,9 +1,18 @@
 package edu.carroll.cs389.json.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+/**
+ * The StockModel class is a JSON model that allows us to turn our JSON strings into
+ * Java objects. We need to do this so Java understands what it's receiving
+ * and storing in the database.
+ *
+ * We use some validation annotations in here: NotNull and Size.
+ *
+ * NotNull: Ensures that the data received is not null.
+ * Size: Ensures that the data received is no longer than 10 characters.
+ */
 public class StockModel {
     @NotNull
     //Future-proof? NYSE currently allows up to 4. Nasdaq currently allows up to 5.
@@ -42,11 +51,17 @@ public class StockModel {
     }
 
     @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
     public String toString() {
-        return "StockModel{" +
-                "ticker='" + ticker + '\'' +
-                ", buyPrice=" + price +
-                ", shares=" + shares +
-                '}';
+        return super.toString();
     }
 }
